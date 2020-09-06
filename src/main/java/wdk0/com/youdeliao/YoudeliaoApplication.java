3602f0963ed555b799be8d82b17575ee1dfd6666
Dikon <wdk_mail@163.com>
2020-09-06 16:36:54 +0800

    init repo

---
+++ src/main/java/wdk0/com/youdeliao/YoudeliaoApplication.java
@@ -0,0 +1,13 @@
+package wdk0.com.youdeliao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoudeliaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YoudeliaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wdk0/com/youdeliao/YoudeliaoApplicationTests.java
@@ -0,0 +1,13 @@
+package wdk0.com.youdeliao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoudeliaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
